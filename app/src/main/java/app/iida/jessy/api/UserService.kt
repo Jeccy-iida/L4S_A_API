d7820d202e138b3ada318acbee1842722163080d
Jeccy-iida <drrr_drrr_4242@docomo.ne.jp>
2021-02-15 02:43:08 +0900

    完成やけどちんぷんかんぷん

---
+++ app/src/main/java/app/iida/jessy/api/UserService.kt
@@ -0,0 +1,13 @@
+package app.iida.jessy.api
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface UserService{
+
+    @GET("users/{userId}")
+    suspend fun getUser(@Path("userId")userId: String): User
+}
+
+
